e5294782b58a869f63a6d66799801e50b57620b6
wilbersx001 <felixenmanuel@hotmail.com>
2023-07-07 14:53:25 -0400

    carpetas agregadas

---
+++ mysql/paralelo/estudiante.sql
@@ -0,0 +1,13 @@
+
+ CREATE TABLE paralelo
+(
+    matricula INTEGER PRIMARY KEY,
+    nombre TEXT
+);
+
+ 
+
+INSERT INTO estudiante(matricula, nombre) VALUES
+ (1,'Felix'), (2,'Enmanuel'), (3,'Rodrigo');
+
+ 
